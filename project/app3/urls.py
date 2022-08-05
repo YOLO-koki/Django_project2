da75eb75d082e6e046a990d24511aada141ad18b
YOLO-koki <masatoshousei@icloud.com>
2022-08-05 23:51:16 +0900

    second commit

--- project/app3/urls.py
+++ project/app3/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Nutrition, NutritionsCreateView
+from .views import Nutrition, NutritionsCreateView, NutritionsUpdateView, NutritionsDeleteView
 
 app_name = 'app3'
 urlpatterns = [
     path('', Nutrition.as_view(), name='nutrition'),
     path('create/', NutritionsCreateView.as_view(), name='create_nutrition'),
+    path('update/<int:pk>', NutritionsUpdateView.as_view(), name='update_nutrition'),
+    path('delete/<int:pk>', NutritionsDeleteView.as_view(), name='delete_nutrition'),
 ]
--- project/app3/views.py
+++ project/app3/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Nutritions
 from .forms import NutritionsForm
 from django.urls import reverse_lazy
@@ -11,7 +11,21 @@ class Nutrition(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["nutritions"] = Nutritions.objects.all()
+        sum_calorie = []
+        sum_protein = []
+        sum_carbohydrate = []
+        sum_lipid = []
+        for i in range(len((Nutritions.objects.values_list('calorie')))):
+            sum_calorie.append(Nutritions.objects.values_list('calorie')[i][0])
+            sum_protein.append(Nutritions.objects.values_list('protein')[i][0])
+            sum_carbohydrate.append(Nutritions.objects.values_list('carbohydrate')[i][0])
+            sum_lipid.append(Nutritions.objects.values_list('lipid')[i][0])
+        context["sum_calorie"] = sum(sum_calorie)
+        context["sum_protein"] = sum(sum_protein)
+        context["sum_carbohydrate"] = sum(sum_carbohydrate)
+        context["sum_lipid"] = sum(sum_lipid)
         return context
+    
 
 class NutritionsCreateView(CreateView):
     model = Nutritions
@@ -19,4 +33,13 @@ class NutritionsCreateView(CreateView):
     form_class = NutritionsForm
     success_url = reverse_lazy('app3:nutrition')
 
-    
+class NutritionsUpdateView(UpdateView):
+    model = Nutritions
+    template_name = "app3/update.html"
+    fields = ['food', 'calorie', 'protein', 'carbohydrate', 'lipid']
+    success_url = reverse_lazy('app3:nutrition')
+
+class NutritionsDeleteView(DeleteView):
+    model = Nutritions
+    template_name = "app3/delete.html"
+    success_url = reverse_lazy('app3:nutrition')
